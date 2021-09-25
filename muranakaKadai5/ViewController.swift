f9f78023f1d3f63423d3120952f672fc42727b9a
akio0911 <akio0911@gmail.com>
2021-09-25 17:23:23 +0900

    feedback

--- muranakaKadai5/ViewController.swift
+++ muranakaKadai5/ViewController.swift
@@ -12,51 +12,52 @@ private enum ErrorMessage {
     static let zeroNum2 = "割る数には、０を入力しないで下さい"
 }
 
+private enum ValidationResult {
+    case valid(Int, Int)
+    case invalid(String)
+}
+
 class ViewController: UIViewController {
     @IBOutlet private weak var calculationLabel: UILabel!
     @IBOutlet private weak var num1TextField: UITextField!
     @IBOutlet private weak var num2TextField: UITextField!
 
     @IBAction private func calculationButton(_ sender: Any) {
-        errorMessageCheck()
-        // ↑↑↑（上記）のメソッドのアラートの処理が一つでも、行われた場合に、
-        // ↓↓↓（下記）のメソッドを行わないようにする処理方法がわかりませんでした。
-        // わからなかったので、calculationLabelに「""」を入れています。
-        guard let calculate = calculate() else {
-            calculationLabel.text = ""
-            return
+        switch validate() {
+        case let .invalid(message):
+            presentAlert(errormessage: message)
+        case let .valid(num1, num2):
+            let result = calculate(num1: num1, num2: num2)
+            calculationLabel.text = String(result)
         }
-        calculationLabel.text = String(calculate)
     }
 
-    private func errorMessageCheck() {
+    private func validate() -> ValidationResult {
         let num1 = Int(num1TextField.text ?? "")
         let num2 = Int(num2TextField.text ?? "")
-        if num1 == nil {
-            message(errormessage: ErrorMessage.nonNum1)
-        } else if num2 == nil {
-            message(errormessage: ErrorMessage.nonNum2)
-        } else if num2 == 0 {
-            message(errormessage: ErrorMessage.zeroNum2)}
+
+        switch (num1, num2) {
+        case (nil, _):
+            return .invalid(ErrorMessage.nonNum1)
+        case (_?, nil):
+            return .invalid(ErrorMessage.nonNum2)
+        case (_?, 0):
+            return .invalid(ErrorMessage.zeroNum2)
+        case (let num1?, let num2?):
+            return .valid(num1, num2)
+        }
     }
 
-    private func message(errormessage: String) {
+    private func presentAlert(errormessage: String) {
         let alert = UIAlertController(title: "課題５", message: "\(errormessage)", preferredStyle: .alert)
-        let textAlert = UIAlertAction(title: "OK", style: .default) { _ in
-            self.dismiss(animated: true, completion: nil)}
+        let textAlert = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.dismiss(animated: true, completion: nil)
+        }
         alert.addAction(textAlert)
         present(alert, animated: true, completion: nil)
     }
 
-    private func calculate() -> Int? {
-        let num1 = Int(num1TextField.text ?? "")
-        let num2 = Int(num2TextField.text ?? "")
-        guard let num1 = num1, let num2 = num2 else {
-            return nil
-        }
-        guard num2 != 0 else {
-            return nil
-        }
-        return num1 / num2
+    private func calculate(num1: Int, num2: Int) -> Int {
+        num1 / num2
     }
 }
